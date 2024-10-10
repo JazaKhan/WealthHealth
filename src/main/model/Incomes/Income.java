9f66ac7c7c80fceb6554dc0add6bb7038a067890
Jaza Khan <jazakhan294@gmail.com>
2024-10-10 00:12:32 -0700

    Added Specification of Income class

---
+++ src/main/model/Incomes/Income.java
@@ -0,0 +1,23 @@
+package model.Incomes;
+
+// Represents a single income with the title and amount (in Dollars)
+public class Income {
+    private String incomeName;
+    private double incomeAmount;
+
+    // REQUIRES: incomeName has a non-zero length.
+    // EFFECTS: Name of the income is set to incomeName, and the amount of
+    // income is set to incomeAmount.
+    public Income(String incomeName, double incomeAmount) {
+        // stub
+    }
+
+    public String getIncomeName() {
+        return null;
+    }
+
+    public double getIncomeAmount() {
+        return 0;
+    }
+
+}
