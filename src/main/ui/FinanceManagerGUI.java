175fd0ed3fc58d5a6ff74401ecb0ee8a006197c0
Jaza Khan <jazakhan294@gmail.com>
2024-11-13 20:04:20 -0800

    Specification for FinanceManagerGUI

--- src/main/ui/FinanceManagerGUI.java
+++ src/main/ui/FinanceManagerGUI.java
@@ -3,7 +3,9 @@ package ui;
 import javax.swing.*;
 import java.awt.*;
 
+import model.Expense;
 import model.ExpensesList;
+import model.Income;
 import model.IncomesList;
 import persistence.JsonReader;
 import persistence.JsonWriter;
@@ -16,7 +18,6 @@ public class FinanceManagerGUI extends JFrame {
 
     private IncomesList incomes;
     private ExpensesList expenses;
-    private boolean isProgramRunning;
     private JsonWriter jsonWriterExp;
     private JsonReader jsonReaderExp;
     private JsonWriter jsonWriterInc;
@@ -43,5 +44,103 @@ public class FinanceManagerGUI extends JFrame {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 
-    
+    //EFFECTS: Creates a header for the GUI Screen
+    public void header(){
+        //stub
+    }
+
+    //EFFECTS: Contains the centered content for the GUI Screen
+    public void content(){
+        //stub
+    }
+
+    //EFFECTS: Creates the buttons required
+    public void buttons(){
+        //stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: adds a new income to the list of incomes
+    public void addIncome() {
+        //stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: adds a new expense to the list of expenses
+    public void addExpense() {
+        //stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: removes an income from the list of incomes
+    public void removeIncome() {
+        //stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: removes an expense from the list of expenses
+    public void removeExpense() {
+        //stub
+    }
+
+    // REQUIRES: incomes > 0
+    // MODIFIES: this
+    // EFFECTS: Changes the income name or amount based on user preferences.
+    public void modifyIncome() {
+        //stub
+    }
+
+    // REQUIRES: expenses > 0
+    // MODIFIES: this
+    // EFFECTS: Changes the expense name or amount based on user preferences.
+    public void modifyExpense() {
+        //stub
+    }
+
+    // EFFECTS: Displays the list of incomes.
+    public void viewIncomesList() {
+        //stub
+    }
+
+    // EFFECTS: Displays the list of expenses.
+    public void viewExpensesList() {
+        //stub
+    }
+
+    // EFFECTS: Saves the program to File
+    public void saveProgram() {
+        //stub
+    }
+
+    // Effects: Reloads progress to program
+    public void reloadProgress() {
+        //stub
+    }
+
+    // EFFECTS: saves the expenses to file
+    private void saveExpenses() {
+        //stub
+    }
+
+    // EFFECTS: saves the incomes to file
+    private void saveIncomes() {
+        //stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: loads expenses from file
+    private void reloadExpenses() {
+        //stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: loads incomes from file
+    private void reloadIncomes() {
+        //stub
+    }
+
+    //EFFECTS: view data in a picture format (working on it still)
+    public void viewData(){
+        //stub
+    }
 }
